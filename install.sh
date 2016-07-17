195650cb2ddeb2be8166f9d1a61cf1ef1df551ff
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2016-07-17 11:14:47 +1000

    Added i3status configuration and relevant installation lines

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # script to symlink i3 configuration files
-
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/i3status
 ln -sv ~/i3/config ~/.config/i3/config
+ln -sv ~/i3/i3status.conf ~/.config/i3status/config
 
